5e2c480ceea7bd81ce6edd9bf49234e4b973cc79
Ansh <anshuman4luv@gmail.com>
2020-02-20 13:47:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var express = require('express');
+var path = require('path');
+
+var app = express();
+app.use(express.static(path.join(__dirname)));
+app.post('/', function (req, res, next) {
+  console.log(JSON.stringify(req.headers));
+  req.pipe(fs.createWriteStream('./'+req.headers['file-name']));
+  req.on('end', next);
+});
+
+app.listen(4000);
